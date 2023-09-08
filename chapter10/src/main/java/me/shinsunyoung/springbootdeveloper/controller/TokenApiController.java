3687e9c522554870ffb03d691f2a9f0642d0dd1c
fanta4715 <fanta4715@naver.com>
2023-09-08 10:07:33 +0900

    TokenApiController만 존재하는 이유

--- chapter10/src/main/java/me/shinsunyoung/springbootdeveloper/controller/TokenApiController.java
+++ chapter10/src/main/java/me/shinsunyoung/springbootdeveloper/controller/TokenApiController.java
@@ -10,10 +10,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RequiredArgsConstructor
 @RestController
 public class TokenApiController {
 
+    //TokenService만 주입한 이유
+    //TokenService는 UserService, RefreshTokenService, TokenProvider를 주입받고 있음
+    //따라서 TokenService만 주입받으면됨.
+    //애초에 RefreshToken을 하든 뭘 하든, 하나의 컨트롤러에 매핑되야 함!!
     private final TokenService tokenService;
 
     @PostMapping("/api/token")
