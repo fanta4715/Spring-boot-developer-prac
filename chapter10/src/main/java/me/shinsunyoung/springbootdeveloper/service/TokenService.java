a13583ba2266633bdfd0c71ac083e4ad81fdef91
fanta4715 <fanta4715@naver.com>
2023-09-08 10:14:13 +0900

    TokenService 흐름 주석

--- chapter10/src/main/java/me/shinsunyoung/springbootdeveloper/service/TokenService.java
+++ chapter10/src/main/java/me/shinsunyoung/springbootdeveloper/service/TokenService.java
@@ -15,6 +15,9 @@ public class TokenService {
     private final RefreshTokenService refreshTokenService;
     private final UserService userService;
 
+    // client <-> resource server(토큰 전체적 다룸) <-> auth server(RefreshToken) 느낌
+    //일단 resource server를 거치고 확인해서, refreshToken을 활용해 auth server에서 토큰을 발급하거나
+    //resource server에서 JWT인증을 끝내거나
     public String createNewAccessToken(String refreshToken) {
         // 토큰 유효성 검사에 실패하면 예외 발생
         if(!tokenProvider.validToken(refreshToken)) {
