db79c3d23d7c00c13550878d28c7758661fb40cc
fanta4715 <fanta4715@naver.com>
2023-09-08 11:07:36 +0900

    jwt 관련 주석

--- chapter10/src/main/java/me/shinsunyoung/springbootdeveloper/service/RefreshTokenService.java
+++ chapter10/src/main/java/me/shinsunyoung/springbootdeveloper/service/RefreshTokenService.java
@@ -8,8 +8,11 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Service
 public class RefreshTokenService {
+
+    //RefreshToken은 AccessToken과는 다르게 DB에 저장을 해야 한다.
     private final RefreshTokenRepository refreshTokenRepository;
 
+    //RefreshToken을 만드는 건 TokenProvider에서 하면 됨
     public RefreshToken findByRefreshToken(String refreshToken) {
         return refreshTokenRepository.findByRefreshToken(refreshToken)
                 .orElseThrow(() -> new IllegalArgumentException("Unexpected token"));
--- chapter10/src/main/java/me/shinsunyoung/springbootdeveloper/service/TokenService.java
+++ chapter10/src/main/java/me/shinsunyoung/springbootdeveloper/service/TokenService.java
@@ -11,22 +11,33 @@ import java.time.Duration;
 @Service
 public class TokenService {
 
+    //토큰을 생성해주기 위함
     private final TokenProvider tokenProvider;
+
+    //RefreshToken이 있는지 확인하기 위함
+    //만드는 건 RefreshToken에서 하지 않음!
     private final RefreshTokenService refreshTokenService;
+
+    //유저 id에 해당하는 유저가 있는지 탐색
     private final UserService userService;
 
     // client <-> resource server(토큰 전체적 다룸) <-> auth server(RefreshToken) 느낌
     //일단 resource server를 거치고 확인해서, refreshToken을 활용해 auth server에서 토큰을 발급하거나
     //resource server에서 JWT인증을 끝내거나
     public String createNewAccessToken(String refreshToken) {
-        // 토큰 유효성 검사에 실패하면 예외 발생
+        // refresh 토큰이 유효한 토큰이 아니면 예외 발생
         if(!tokenProvider.validToken(refreshToken)) {
             throw new IllegalArgumentException("Unexpected token");
         }
 
+        // refreshToken이 유효하다면
+        // String을 활용해서 RefreshToken 객체를 받고, 거기서 user ID를 얻어냄
         Long userId = refreshTokenService.findByRefreshToken(refreshToken).getUserId();
+
+        //유저 id로 User를 얻어냄
         User user = userService.findById(userId);
 
+        //user에 해당하는 토큰을 생성해서 리턴 (AccessToken을 리턴하는 것)
         return tokenProvider.generateToken(user, Duration.ofHours(2));
     }
 }
